67bf60e1e3ea9368474045fbb20e5264ab302df4
devig <golovnevivan@mail.ru>
2020-06-08 12:01:40 +0300

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
               //if (env('APP_ENV') !== 'local') {
-                    $url->forceScheme('https');
+                    URL::forceScheme('https');
               //}
     }
 
